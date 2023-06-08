82e4ea300085f7482511640279da783bcf4b1d5e
Helena Xu <hxu1617@students.cs.ubc.ca>
2023-06-07 23:05:35 -0700

    Move action buttons

--- frontend/src/components/PropertyDetailCard.js
+++ frontend/src/components/PropertyDetailCard.js
@@ -1,13 +1,13 @@
 import "./PropertyDetailCard.css";
-
 function PropertyDetailCard({id, name, address}) {
     return (
-        <div className="property-card">
-            <header>{name ? name : id}</header>
-            <p className="property-address">{address.streetAddress}</p>
-            <p className="property-zone-info"><small>{address.city}, {address.province} {address.zipCode}</small></p>
-            <p><small><small>ID#: {id}</small></small></p>
-        </div>
+            <div className="property-card">
+                <div className="property-detail">
+                    <header>{name ? name : id}</header>
+                    <p className="property-address">{address.streetAddress}</p>
+                    <p className="property-zone-info"><small>{address.city}, {address.province} {address.zipCode}</small></p>
+                </div>
+            </div>
     );
 }
 export default PropertyDetailCard;
