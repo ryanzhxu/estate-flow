817fb9743d0b1a2179f8af2e1605cebe112067d5
Helena Xu <hxu1617@students.cs.ubc.ca>
2023-06-07 23:05:35 -0700

    Move action buttons

--- frontend/src/components/PropertyHome.js
+++ frontend/src/components/PropertyHome.js
@@ -3,20 +3,26 @@ import PropertyOverview from "./PropertyOverview";
 import PropertyPhotoGallery from "./PropertyPhotoGallery";
 import "./PropertyHome.css";
 
+
 function PropertyHome({property}) {
     return (
         <div>
-            <PropertyDetailCard address={property.address} id={property.id} name={property.name}/>
-            <div className="property-home-main-content">
-                <div className="property-overview-container">
-                    <PropertyOverview property={property}/>
-                </div>
+            <div
+                className="property-card"
+                id="property-home-header"
+            >
+                <PropertyDetailCard address={property.address} id={property.id} name={property.name}/>
                 <div className="property-actions-container">
                     <div className="property-actions">
                         <button className="property-action">Edit Tenants</button>
                         <button className="property-action">Edit Details</button>
                         <button className="property-action">Calculate Profit</button>
                     </div>
+                </div>
+            </div>
+            <div className="property-home-main-content">
+                <div className="property-overview-container">
+                    <PropertyOverview property={property}/>
                 </div>
                 <div className="gallery-container">
                     <PropertyPhotoGallery photos={property.photos}/>
