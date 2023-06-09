183d3ecd821d965d6431518802da5b980c4b8576
Ryan Xu <ryan.xu@Ryans-Air.lan>
2023-06-08 19:58:11 -0700

    EF-28: Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,11 @@
 import './App.css';
-// import logo from './logo.svg';
 import React from 'react';
-import InputForm from './shared/components/InputForm';
-import LeaseFormInputs from './shared/constants/input/LeaseFormInputs';
+import HomeWelcomePage from './components/home/HomeWelcomePage';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <InputForm fields={Object.values(LeaseFormInputs.Required)} />
+    <HomeWelcomePage />
+    // <InputForm fields={Object.values(LeaseFormInputs.Required)} />
   );
 }
 
