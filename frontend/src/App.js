8c79c448e2ae0e1f0775d4236d014793a007d7b4
luongn <luongn@students.cs.ubc.ca>
2023-06-08 22:25:56 -0700

    hopefully fixed conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,11 @@
 import './App.css';
-// import logo from './logo.svg';
 import React from 'react';
-// import InputForm from './shared/components/InputForm';
-// import LeaseFormInputs from './shared/constants/input/LeaseFormInputs';
-import PropertyInputForm from './shared/constants/PropertyStuff/PropertyInputForm';
-import PropertyFormInputs from './shared/constants/PropertyStuff/PropertyFormInputs';
+import HomeWelcomePage from './components/home/HomeWelcomePage';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <div>
-    <h1>Property Form</h1>
-    <PropertyInputForm inputs={PropertyFormInputs} />
-  </div>
+    <HomeWelcomePage />
+    // <InputForm fields={Object.values(LeaseFormInputs.Required)} />
   );
 }
 
