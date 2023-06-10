6594941455adfcc290246f4dc310f982ff89869f
Ryan Xu <ryan.xu@Ryans-Air.lan>
2023-06-09 20:12:19 -0700

    added line back

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import React from 'react';
 import HomeWelcomePage from './components/home/HomeWelcomePage';
+import { useSelector } from 'react-redux';
+import LoginScreen from './components/login/LoginScreen';
+import { selectUser } from './reducers/userSlice';
 
 function App() {
   const user = useSelector(selectUser);
 
   return (
+    <div>{user ? <HomeWelcomePage /> : <LoginScreen />}</div>
     // <InputForm fields={Object.values(LeaseFormInputs.Required)} />
   );
 }
