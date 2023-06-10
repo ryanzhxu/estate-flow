5230fb72455c096eebb278b75afed5165982ad92
Ryan Xu <ryan.xu@Ryans-Air.lan>
2023-06-09 22:21:08 -0700

    code clean up

--- frontend/src/components/propertyhome/PropertyHome.js
+++ frontend/src/components/propertyhome/PropertyHome.js
@@ -3,39 +3,40 @@ import PropertyOverview from "./PropertyOverview";
 import PropertyPhotoGallery from "./PropertyPhotoGallery";
 import "./PropertyHome.css";
 import TenantView from "./TenantView";
+import React from "react";
 
-
-function PropertyHome({property}) {
-    return (
-        <div className="home-container">
-            <div
-                className="property-card"
-                id="property-home-header"
-            >
-                <PropertyDetailCard address={property.address} id={property.id} name={property.name}/>
-                <div className="property-actions-container">
-                    <div className="property-actions">
-                        <button className="property-action">Edit Tenants</button>
-                        <button className="property-action">Edit Details</button>
-                        <button className="property-action">Calculate Profit</button>
-                    </div>
-                </div>
-            </div>
-            <div className="property-home-main-content-container">
-                <div className="property-home-main-content">
-                    <div className="property-overview-container">
-                        <PropertyOverview property={property}/>
-                    </div>
-                    <div className="gallery-container">
-                        <PropertyPhotoGallery photos={property.photos}/>
-                    </div>
-                </div>
-                <div className="tenants-view-container border-left">
-                    <TenantView tenants={property.tenants}/>
-                </div>
-            </div>
+function PropertyHome({ property }) {
+  return (
+    <div className="home-container">
+      <div className="property-card" id="property-home-header">
+        <PropertyDetailCard
+          address={property.address}
+          id={property.id}
+          name={property.name}
+        />
+        <div className="property-actions-container">
+          <div className="property-actions">
+            <button className="property-action">Edit Tenants</button>
+            <button className="property-action">Edit Details</button>
+            <button className="property-action">Calculate Profit</button>
+          </div>
         </div>
-    )
+      </div>
+      <div className="property-home-main-content-container">
+        <div className="property-home-main-content">
+          <div className="property-overview-container">
+            <PropertyOverview property={property} />
+          </div>
+          <div className="gallery-container">
+            <PropertyPhotoGallery photos={property.photos} />
+          </div>
+        </div>
+        <div className="tenants-view-container border-left">
+          <TenantView tenants={property.tenants} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PropertyHome;
--- frontend/src/shared/constants/PropertyStuff/PropertyInputForm.jsx
+++ frontend/src/shared/constants/PropertyStuff/PropertyInputForm.jsx
@@ -4,14 +4,17 @@ const renderFormInputs = (inputs) => {
   return Object.keys(inputs).map((section) => {
     return (
       <div key={section}>
-        <h2 className = "sectionTitle">{section}</h2>
+        <h2 className="sectionTitle">{section}</h2>
         {Object.keys(inputs[section]).map((input) => {
           const field = inputs[section][input];
-          if (field.hasOwnProperty("options")) { // basically just for Property Type, anything with Options 
+          if (field.hasOwnProperty("options")) {
+            // basically just for Property Type, anything with Options
             return (
-              <div key={input} className = "inputOptions">
-                <label htmlFor={input} className = "optionsLabel">{field.label}</label>
-                
+              <div key={input} className="inputOptions">
+                <label htmlFor={input} className="optionsLabel">
+                  {field.label}
+                </label>
+
                 <br></br>
                 <select name={input}>
                   {field.options.map((option) => (
@@ -21,20 +24,25 @@ const renderFormInputs = (inputs) => {
               </div>
             );
           }
-          if (field.hasOwnProperty("file")) { // just for photos
+          if (field.hasOwnProperty("file")) {
+            // just for photos
             return (
-              <div key={input} className = "inputFile">
-                <label htmlFor={input} className = "fileLabel">{field.label}</label>
+              <div key={input} className="inputFile">
+                <label htmlFor={input} className="fileLabel">
+                  {field.label}
+                </label>
                 <br></br>
-                <input type="file" id={input} name={input}/>
+                <input type="file" id={input} name={input} />
               </div>
             );
           }
           return (
-            <div key={input} className = "inputText">
-              <label htmlFor={input} className = "textLabel">{field}</label>
+            <div key={input} className="inputText">
+              <label htmlFor={input} className="textLabel">
+                {field}
+              </label>
               <br></br>
-              <input type="text" id={input} name={input}/>
+              <input type="text" id={input} name={input} />
             </div>
           );
         })}
@@ -43,15 +51,18 @@ const renderFormInputs = (inputs) => {
   });
 };
 
-const PropertyInputForm = ({ inputs }) => { // submit doesnt do anything yet
+const PropertyInputForm = ({ inputs }) => {
+  // submit doesnt do anything yet
   return (
     <div className="property-form">
-    {renderFormInputs(inputs)}
-    <br></br>
-    <div className="submitButtonContainer">
-      <button type="submit" className="submitButton">Submit</button> 
-    </div>
-    <br></br>
+      {renderFormInputs(inputs)}
+      <br></br>
+      <div className="submitButtonContainer">
+        <button type="submit" className="submitButton">
+          Submit
+        </button>
+      </div>
+      <br></br>
     </div>
   );
 };
