330104439f52972802305062c865005947e360a1
Helena Xu <hxu1617@students.cs.ubc.ca>
2023-06-27 20:57:53 -0700

    Added example endpoint using DB

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const property = require("../models/property") // TO BE REMOVED
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({ title: 'Express' });
+});
+
+/*
+  EXAMPLE: TO BE REMOVED
+ */
+router.get('/demo', async function(req, res, next) {
+  try {
+    const properties = await property.find();
+    res.json(properties);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json(e);
+  }
+
 });
 
 module.exports = router;
