d4e37a470dae2fc053f73f495fe5304125813734
Helena Xu <hxu1617@students.cs.ubc.ca>
2023-07-03 23:39:17 -0400

    Add post endpoint for tenants

--- server/routes/tenant.js
+++ server/routes/tenant.js
@@ -15,10 +15,21 @@ router.get("/tenants", async(req, res) => {
 router.get("/properties/:id/tenants", async(req, res) => {
     const propertyId = req.params.id;
     try {
-        res.status(200).json(await Tenant.find({propertyId: propertyId}));
+        const tenants = await Tenant.find({propertyId: propertyId});
+        res.status(200).json(tenants);
     } catch(e) {
         console.error(e);
         res.status(500).json({error: "Error accessing tenants"});
+    }
+})
+
+router.post("/properties/:id/tenant", async(req, res) => {
+    const tenant = new Tenant(req.body);
+    try {
+        res.status(201).json(await tenant.save());
+    } catch(e) {
+        console.error("What's the problem?", e);
+        res.status(500).json({error: "Error saving tenant to property"})
     }
 })
 
