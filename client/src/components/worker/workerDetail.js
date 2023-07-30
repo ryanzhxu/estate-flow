141fea4ce19fc561848f7b534ce6b545af5a4e58
Zichao Zhou <zichao16@students.cs.ubc.ca>
2023-07-29 22:26:12 -0700

    functional1

--- client/src/components/worker/workerDetail.js
+++ client/src/components/worker/workerDetail.js
@@ -6,6 +6,16 @@ import './worker.css';
 const WorkerDetail = () => {
   const select = useSelector(expSelectedWorker);
   const dispatch = useDispatch();
+  let streetAddress = ""
+  let city = ""
+  let province = ""
+  let postalCode = ""
+  if(select.address){
+    streetAddress = select.address.streetAddress? select.address.streetAddress : ""
+    city = select.address.city? select.address.city : ""
+    province = select.address.province? select.address.province : ""
+    postalCode = select.address.postalCode? select.address.postalCode : ""
+  }
   return (
     <div className='workerDetail'>
       <div className='details'>
@@ -14,22 +24,19 @@ const WorkerDetail = () => {
             <p className='single-line'>Name:</p>
             <p className='single-line'>Email:</p>
             <p className='single-line'>Phone:</p>
-            <p className='single-line'>Address:</p>
             <p className='single-line'>Hourly rate:</p>
             <p className='single-line'>Trades:</p>
-            <p className='single-line'>Postal code:</p>
+            <p className='single-line'>Address:</p>
+            <p className='single-line'></p>
           </div>
           <div>
-            <p>{select.firstName}</p>
-            <p>{select.lastName}</p>
+            <p>{select.firstName} {select.lastName}</p>
             <p>{select.email}</p>
             <p>{select.phoneNumber}</p>
-            <p>${select.address.streetAddress}/hr</p>
-            <p>{select.address.city}</p>
-            <p>{select.address.province}</p>
-            <p>{select.address.postalCode}</p>
             <p>{select.hourlyRate}</p>
             <p>{select.trades}</p>
+            <p>{streetAddress}</p>
+            <p>{city}, {province}, {postalCode}</p>
           </div>
         </div>
         <div className='worker-detail-buttons'>
