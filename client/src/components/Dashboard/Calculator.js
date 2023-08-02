b10b6021859c392eed9c6387d6ba1f02235c8401
Helena Xu <hxu1617@students.cs.ubc.ca>
2023-08-02 14:42:58 -0400

    Add additional styling for Calculator

--- client/src/components/Dashboard/Calculator.js
+++ client/src/components/Dashboard/Calculator.js
@@ -3,16 +3,15 @@ import React, {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {getPropertiesForDashboardAsync} from "../../redux/properties/thunks";
 import StatsService from "../../redux/stats/service"
-import {Button, Modal} from "react-bootstrap";
 import {
-    Checkbox,
+    Checkbox, createTheme,
     IconButton,
     InputAdornment,
     List,
     ListItem,
     ListItemButton,
     ListItemIcon, ListItemText,
-    TextField
+    TextField, ThemeProvider
 } from "@mui/material";
 import ProfitModal from "./ProfitModal";
 
@@ -51,6 +50,7 @@ function Calculator() {
     const [isOpen, setIsOpen] = useState(false);
     const [totalRent, setTotalRent] = useState(0);
     const [mortgageValue, setMortgageValue] = useState("");
+    const [isFocused, setIsFocused] = useState(false);
 
     const handleCalculate = async () => {
         if (checked.length > 0) {
@@ -60,89 +60,99 @@ function Calculator() {
         }
     };
 
-    const handleClose = () => {
-        setIsOpen(false);
-    };
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#032B43'
+            }
+        },
+    });
 
     return (
-        <motion.div>
+        <ThemeProvider theme={theme}>
             <motion.div>
-                <TextField
-                    id="outlined-basic"
-                    label="Mortgage"
-                    variant="outlined"
-                    size="small"
-                    type="number"
-                    value={mortgageValue}
-                    onChange={(e) => setMortgageValue(e.target.value)}
-                    inputProps={{
-                        min: 0,
-                        step: 0.01,
-                    }}
-                    InputProps={{startAdornment: <InputAdornment position="start">$</InputAdornment>}}
-                />
-                <IconButton style={{ width: "40px", height: "40px", borderRadius: "50%" }} onClick={handleCalculate}>
-                    <i className="bi bi-calculator bi-sm"/>
-                </IconButton>
-                <ProfitModal
-                    mortgageValue={mortgageValue === "" ? 0 : mortgageValue}
-                    totalRent={totalRent}
-                    isOpen={isOpen}
-                    onClose={() => setIsOpen(false)}
-                />
-            </motion.div>
-            <motion.div style={{ maxHeight: '420px', overflowY: 'auto' }}>
-                <List sx={{ width: '100%', maxWidth: 360, bgcolor: '#DEE2E6' }}>
-                    <ListItem disablePadding>
-                        <ListItemButton role={undefined} onClick={handleToggleAll} dense>
-                            <ListItemIcon>
-                                <Checkbox
-                                    edge="start"
-                                    checked={selectAllChecked}
-                                    tabIndex={-1}
-                                    disableRipple
-                                    inputProps={{ 'aria-labelledby': 'select-all-label' }}
-                                    style ={{
-                                        color: "#C9A461",
-                                    }}
-                                />
-                            </ListItemIcon>
-                            <ListItemText id="select-all-label" primary="Select All" />
-                        </ListItemButton>
-                    </ListItem>
-                    {properties.map((property, i) => {
-                        const labelId = `checkbox-list-label-${property}`;
-
-                        return (
-                            <ListItem
-                                key={i}
-                                disablePadding
-                            >
-                                <ListItemButton role={undefined} onClick={handleToggle(property._id)} dense>
-                                    <ListItemIcon>
-                                        <Checkbox
-                                            edge="start"
-                                            checked={checked.some((id) => id === property._id)}
-                                            tabIndex={-1}
-                                            disableRipple
-                                            inputProps={{ 'aria-labelledby': labelId }}
-                                            style ={{
-                                                color: "#032B43",
-                                            }}
-                                        />
-                                    </ListItemIcon>
-                                    <ListItemText
-                                        id={labelId}
-                                        primary={`${property.address.streetAddress}, ${property.address.city}, 
-                                    ${property.address.province} ${property.address.postalCode}`}
+                <motion.div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <TextField
+                        id="outlined-basic"
+                        label="Mortgage"
+                        variant="outlined"
+                        size="small"
+                        type="number"
+                        color="primary"
+                        value={mortgageValue}
+                        onFocus={() => setIsFocused(true)}
+                        onBlur={() => setIsFocused(false)}
+                        onChange={(e) => setMortgageValue(e.target.value)}
+                        inputProps={{
+                            min: 0,
+                            step: 0.01,
+                        }}
+                        InputProps={{startAdornment: isFocused || mortgageValue !== "" ? <InputAdornment position="start">$</InputAdornment> : null}}
+                        style={{ width: "70%" }}
+                    />
+                    <IconButton style={{ width: "40px", height: "40px", borderRadius: "50%" }} onClick={handleCalculate}>
+                        <i className="bi bi-calculator bi-sm"/>
+                    </IconButton>
+                    <ProfitModal
+                        mortgageValue={mortgageValue === "" ? 0 : mortgageValue}
+                        totalRent={totalRent}
+                        isOpen={isOpen}
+                        onClose={() => setIsOpen(false)}
+                    />
+                </motion.div>
+                <motion.div style={{ maxHeight: '420px', overflowY: 'auto' }}>
+                    <List sx={{ width: '100%', maxWidth: 360, bgcolor: '#DEE2E6' }}>
+                        <ListItem disablePadding>
+                            <ListItemButton role={undefined} onClick={handleToggleAll} dense>
+                                <ListItemIcon>
+                                    <Checkbox
+                                        edge="start"
+                                        checked={selectAllChecked}
+                                        tabIndex={-1}
+                                        disableRipple
+                                        inputProps={{ 'aria-labelledby': 'select-all-label' }}
+                                        style ={{
+                                            color: "#C9A461",
+                                        }}
                                     />
-                                </ListItemButton>
-                            </ListItem>
-                        );
-                    })}
-                </List>
+                                </ListItemIcon>
+                                <ListItemText id="select-all-label" primary="Select All" />
+                            </ListItemButton>
+                        </ListItem>
+                        {properties.map((property, i) => {
+                            const labelId = `checkbox-list-label-${property}`;
+
+                            return (
+                                <ListItem
+                                    key={i}
+                                    disablePadding
+                                >
+                                    <ListItemButton role={undefined} onClick={handleToggle(property._id)} dense>
+                                        <ListItemIcon>
+                                            <Checkbox
+                                                edge="start"
+                                                checked={checked.some((id) => id === property._id)}
+                                                tabIndex={-1}
+                                                disableRipple
+                                                inputProps={{ 'aria-labelledby': labelId }}
+                                                style ={{
+                                                    color: "#032B43",
+                                                }}
+                                            />
+                                        </ListItemIcon>
+                                        <ListItemText
+                                            id={labelId}
+                                            primary={`${property.address.streetAddress}, ${property.address.city}, 
+                                    ${property.address.province} ${property.address.postalCode}`}
+                                        />
+                                    </ListItemButton>
+                                </ListItem>
+                            );
+                        })}
+                    </List>
+                </motion.div>
             </motion.div>
-        </motion.div>
+        </ThemeProvider>
     );
 }
 
