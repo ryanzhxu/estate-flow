bc2f4401f1f7b0ea9ed1eed07b41b56c1ed5ccf5
Helena Xu <hxu1617@students.cs.ubc.ca>
2023-08-03 00:01:55 -0400

    Adjust path

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client', 'public')));
 
 app.use('/users', usersRouter);
 app.use(propertiesRouter);
@@ -29,7 +29,7 @@ app.use(tenantsRouter);
 app.use(statsRouter);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'));
 });
 
 module.exports = app;
