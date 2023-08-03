1c71a30aa6644c30368cd906ab35f471a2704c25
Helena Xu <hxu1617@students.cs.ubc.ca>
2023-08-02 23:14:35 -0400

    Add catch-all on server-side for client rendering

--- server/app.js
+++ server/app.js
@@ -22,11 +22,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use(propertiesRouter);
 app.use(workersRouter);
 app.use(tenantsRouter);
 app.use(statsRouter);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 module.exports = app;
