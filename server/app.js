f9ea1f1a6a019df461eda6f7c201ad548aa0601b
Helena Xu <hxu1617@students.cs.ubc.ca>
2023-08-03 00:22:51 -0400

    Attempt 4

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.use(tenantsRouter);
 app.use(statsRouter);
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'), function (err) {
+    res.sendFile(path.join(__dirname, 'build/index.html'), function (err) {
         if (err) {
             res.status(500).send(err)
         }
