b16eed125bb09b874282205a5bbc2ce67baaaf04
yli0402 <77415930+yli0402@users.noreply.github.com>
2023-08-04 00:19:15 +0800

    deleted pdf sections in dashboard

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -6,19 +6,11 @@ const Dashboard = () => {
   return (
     <div className='body_container'>
       <motion.div className='body_card'>
-        <motion.div>Bills Tracking{<br />}PDF</motion.div>
-      </motion.div>
-
-      <motion.div className='body_card'>
-        <motion.div>Lease Tracking{<br />}PDF</motion.div>
-      </motion.div>
-
-      <motion.div className='body_card'>
         <Calendar />
       </motion.div>
 
       <motion.div className='body_card'>
-        <motion.div >Calculator</motion.div>
+        <motion.div>Calculator</motion.div>
       </motion.div>
     </div>
   );
