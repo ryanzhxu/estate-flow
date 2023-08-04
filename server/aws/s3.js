227648fc7390478a792d8d0e8ed39a796cae1cd0
Helena Xu <hxu1617@students.cs.ubc.ca>
2023-08-04 02:15:34 -0400

    Set up aws upload to allow multiple file upload

--- server/aws/s3.js
+++ server/aws/s3.js
@@ -4,32 +4,17 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-// aws.config.update({
-//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//     region: "us-east-1"
-// })
-
-const upload =  async(file, folder) => {
+const upload =  async(files, folder) => {
     const s3 = new aws.S3();
-    const param = {
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Key: `uploads/${folder}/${uuid()}-${file.originalname}`,
-        Body: file.buffer
-    }
-    return await s3.upload(param).promise();
+    const params = files.map((file) => {
+        return {
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Key: `uploads/${folder}/${uuid()}-${file.originalname}`,
+            Body: file.buffer
+        }
+    });
+    return await Promise.all(params.map((param) => s3.upload(param).promise()))
 }
-
-// const upload = multer({
-//     storage: multerS3({
-//         s3: new aws.S3(),
-//         bucket: process.env.AWS_BUCKET_NAME,
-//         key: (req, file, cb) => {
-//             console.log(file);
-//             cb(null, uuid() + "-" + file.originalname); //use Date.now() for unique file keys
-//         }
-//     })
-// });
 
 module.exports = {
     s3upload: upload
