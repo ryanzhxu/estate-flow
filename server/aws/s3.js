ba77b644c89cf89cca25241b63d2e09818d70bc9
Helena Xu <hxu1617@students.cs.ubc.ca>
2023-08-04 23:08:38 -0400

    Add method for deleting files in AWS

--- server/aws/s3.js
+++ server/aws/s3.js
@@ -1,22 +1,46 @@
-const aws = require("aws-sdk");
+const {S3} = require("aws-sdk");
 const { v4: uuid } = require('uuid');
 const dotenv = require('dotenv');
+const path = require("path");
 
 dotenv.config();
+const s3 = new S3();
 
 const upload =  async(files, folder) => {
-    const s3 = new aws.S3();
     const params = files.map((file) => {
+        const fileExtension = path.extname(file.originalname);
         return {
             Bucket: process.env.AWS_BUCKET_NAME,
-            Key: `uploads/${folder}/${uuid()}-${file.originalname}`,
+            Key: `uploads/${folder}/${uuid()}${fileExtension}`,
             Body: file.buffer
         }
     });
     return await Promise.all(params.map((param) => s3.upload(param).promise()))
 }
 
+const deleteFiles = async(urls) => {
+    const keys = urls.map((url) => {
+        return {
+            Key: new URL(url).pathname.slice(1)
+        }
+    });
+
+    const param = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Delete: {
+            Objects: keys
+        }
+    }
+
+    await s3.deleteObjects(param).promise()
+        .then((deletedFiles) => {
+            console.log(deletedFiles)
+        });
+
+}
+
 module.exports = {
-    s3upload: upload
+    s3upload: upload,
+    deleteFiles
 }
 
