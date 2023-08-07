f45061b932b98347ba49ae0c8681e69d83959f43
luongn <luongn@students.cs.ubc.ca>
2023-08-07 11:52:24 -0700

    added banner to dashboard
    added banner to dashboard

---
+++ client/src/components/Dashboard/Banner.js
@@ -0,0 +1,16 @@
+import './Banner.css'
+export default function Banner() {
+    return (
+        <div>
+        <h3>Estate Flow - Simplify Your Property Management</h3>
+          <p>
+            Estate Flow is a comprehensive web app designed for property managers, offering seamless management of
+            rental properties, houses, townhouses, condos, commercial spaces, and offices. This app efficiently stores
+            and organizes data related to properties, buildings, land, tenants, managers, maintenance workers, and more.
+            With Estate Flow, users can effortlessly perform CRUD operations, and track various fees, leases, and
+            expenses. Our goal is to empower property managers to maximize their efficiency and profitability while
+            ensuring the security and privacy of sensitive data.
+          </p>
+        </div>
+    )
+}
