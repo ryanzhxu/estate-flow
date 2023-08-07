d5a7f2a232bcf1b8ee19697604acb9b2c05f7b62
Helena Xu <hxu1617@students.cs.ubc.ca>
2023-08-06 23:00:18 -0400

    Add file filter to limit file size and type

--- server/aws/multer.js
+++ server/aws/multer.js
@@ -1,5 +1,20 @@
 const multer = require("multer");
 const storage = multer.memoryStorage();
-const upload = multer({storage: storage});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+        cb(null, true);
+    } else {
+        cb(new Error('Only image files of type jpg, png are allowed'), false);
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 5 * 1024 * 1024 // 5MB
+    }
+});
 
 module.exports = upload;
